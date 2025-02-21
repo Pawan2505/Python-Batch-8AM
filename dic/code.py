8b99a9ccbeb615b6c77b03e7e0ea338afc159879
pawan2505 <pawanaktu@gmail.com>
2025-02-21 13:37:07 +0530

    Python

---
+++ dic/code.py
@@ -0,0 +1,125 @@
+# print("Hello")
+
+
+# my_dic = {
+#     "name":"Pawan",
+#     "salary":123,
+#     "id":12
+# }
+
+# print(my_dic)
+
+# print(my_dic["salary"])
+
+# my_dic["name"] = "utkarsh"
+
+# print(my_dic)
+
+# my_dic["subject"] = "Python"
+
+# print(my_dic)
+
+# remove_data = my_dic.pop("salary")
+
+# print(my_dic)
+
+# print(remove_data)
+
+
+# print(my_dic.keys())
+
+# print(my_dic.values())
+
+
+# nested dictionary
+
+# person = {
+#     "name" : "Pawan",
+#     "address":{
+#         "street":"123 dfwejf",
+#         "city":"Azamgarh"
+#     }
+# }
+
+# # print(person)
+
+# print(person["address"] ["street"])
+
+
+# dictionary = {
+#     "fruits":["apple","banana","cherry"],
+#     "vegitable":["carrot","hdgw"]
+# }
+
+# print(dictionary)
+
+# dictionary["fruits"].append("orange")
+
+# print(dictionary["fruits"])
+
+# print(dictionary)
+
+
+# dictionary = {
+#     "fruits":{"apple","banana","cherry"},
+#     "vegitable":{"carrot","hdgw"}
+# }
+
+# print(dictionary)
+
+# dictionary["fruits"].add("orange")
+
+# print(dictionary["fruits"])
+
+# print(dictionary)
+
+
+# CONDITIONAL STATEMENT
+
+# 1. if
+# 2. if else
+# 3. if-elif-else
+
+
+# if
+
+# if condition:
+#     # code to execute if condition true
+
+# age = 17
+
+# if (age>18):
+#     print("major")
+# else:
+#     print("minor")
+
+
+# if-elif-else
+
+# if condition1:
+#     # code to execute if condition is true
+# elif condition2:
+#     #code to execute if condition2 is true
+# else:
+#     #code to execute if none of the above condition are true
+
+
+# age = 13
+
+# if age<13:
+#     print("child")
+# elif age<18:
+#     print("teenager")
+# else:
+#     print("adult")
+
+
+# Nested if statement
+
+age = 25
+
+if age>=18:
+    if age<21:
+        print("you are not eligible for marrage")
+    else:
+        print("You are eligible for marrage")
