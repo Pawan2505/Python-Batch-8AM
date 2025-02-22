5e38a6bceee09126b12506e6f94da4cd079668b7
pawan2505 <pawanaktu@gmail.com>
2025-02-22 09:45:12 +0530

    Loop

---
+++ 09_Loops/01_For Loops/practice.py
@@ -0,0 +1,152 @@
+
+# for loop
+
+# for variable in sequence:
+#     #code
+
+
+# l = [11,22,33,44,55,66]
+
+# for item in l:
+#     print(item)
+
+
+# for i in range(5):
+#     print(i)
+
+# print(range(5))
+
+# name = "Pawan"
+
+# for char in name:
+#     print(char)
+
+
+# for num in range(5):
+#     print(num)
+# else:
+#     print("Loop Completed")
+
+
+# for num in range(5):
+#     if num==3:
+#         break
+#     print(num)
+
+
+
+# for num in range(5):
+#     if num==3:
+#         continue
+#     print(num)
+# else:
+#     print("Loop Completed")
+
+# Taking Multiple input using for loop
+
+# n = int(input("Enter the number of elements: "))
+
+# number = []
+
+# for i in range(n):
+#     num = int(input(f"Enter number {i} : "))
+#     number.append(num)
+
+# print("You enterd : ",number)
+
+# print(number.index(25))
+
+
+
+# n = int(input("Enter the number of elements: "))
+
+# number = []
+
+# for i in range(10,5,-1):
+#     num = int(input(f"Enter number {i} : "))
+#     number.append(num)
+
+# print("You enterd : ",number)
+
+# print(number.index(25))
+
+
+# num = 34
+
+# print(f"The value of num is {num} . and the..")
+
+
+# .format()
+
+# Positional Argument
+
+# print("My name is {} and i am {} year old".format("Pawan",24))
+
+# Named Argument
+
+# print("My name is {age} and i am {name} year old".format(name="Pawan",age=24))
+
+# Formatting Number
+
+# print("The price is {:.1f} rupees".format(2534.63982578))
+
+# while loop
+
+# while condition:
+#     #code to execute 
+
+
+# count = 1
+
+# while (count <=10):
+#     print(count)
+#     count +=1
+
+
+# count = 1
+
+# while (count <=10):
+#     print(count)
+#     count +=1
+# else:
+#     print("loop ended")
+
+# count = 1
+
+# while (count <=10):
+#     if count == 5:
+#         break
+#     print(count)
+#     count +=1
+# else:
+#     print("loop ended")
+
+
+# count = 0
+
+# while (count <10):
+# count +=1
+#     if count == 5:
+#         continue
+#     print(count)
+
+
+# x = 0
+
+# while x < 5:
+#     x +=1
+#     if x == 3:
+#         continue
+#     print(x)
+
+
+# import time
+
+# countdown = 5
+
+# while countdown>0:
+#     print("Time left : ",countdown)
+#     time.sleep(2) # maintain speed of print
+#     countdown -= 1
+# print("Time's up!")
+
